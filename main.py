cc657ea877c4500142c49837035aa7a4b9038f5d
akashmauryanitsgr <akashmaurya18ap@gmail.com>
2025-04-21 13:41:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import eel
+import sys
+#sys.path.append(os.path.dirname(os.path.abspath('D:\\endterm\\jarvis\\main.py')))
+
+from engine.features import *
+from engine.command import *
+    
+def start():
+    
+  eel.init("www")
+
+  #playAssistantSound()
+  os.system('start chrome.exe --app=" http://127.0.0.1:5501//jarvis/www/index.html"')
+  
+  eel.start('index.html', mode="chrome", host='localhost', block=True)
+ 
+  
+
+    
---
+++ run.py
@@ -0,0 +1,27 @@
+import multiprocessing
+
+
+def startJarvis():
+    print("Process1 is running")
+    from main import start
+    start()
+    
+def listenHotword():
+    print("process 2 is running...") 
+    from engine.features import hotword
+    hotword()   
+    
+    
+if __name__ == '__main__':
+    p1 = multiprocessing.Process(target=startJarvis) 
+    
+    p2 = multiprocessing.Process(target=listenHotword) 
+    p1.start()
+    p2.start()
+    p1.join()
+    
+    if p2.is_alive():
+        p2.terminate()
+        p2.join()
+        
+    print("system stop")       
